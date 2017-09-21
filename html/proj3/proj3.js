e6a02108528cca1dcc6bdcf5a47fc26397c32a3b
mfergu <mfergu3@g.clemson.edu>
2017-09-21 13:31:47 -0400

    checking user input
    needs to implement UI

--- html/proj3/proj3.js
+++ html/proj3/proj3.js
@@ -1,78 +1,36 @@
-//setup document
 console.log("connected!");
-document.querySelector("title").innerText = " Project 3";
 var body = document.querySelector("body");
+var input_num;
+
+//create title
+document.querySelector("title").innerText = " Project 3";
+
+//create user input
 var num_input = document.createElement("input");
-num_input.type = "string";
-num_input.value = "";
+num_input.type = "number";
+num_input.pattern = "[0-9]";
+num_input.max = "999999999999999";
 body.appendChild(num_input);
 
-//global vars for state of items
-var total_nums = 0;
-var buttons = [[],[],[],[],[]];
-var display_str = "";
-
-//balances user and CPU input 
-var clicker = function(add_to_display){
-	total_nums++;
-	display_str += add_to_display;
-	document.querySelector("input").value = display_str;
-	console.log(total_nums + " is size of input");
-	console.log( display_str + " is the total number");
-	console.log(add_to_display + " is added to the display_str");
+//verify user input 
+var verify = function(){
+    input_num = document.querySelector("input");
+    console.log(input_num.value);
+    if( input_num.value.toString().length > 15 ) {
+        console.log("input too long");
+    } else if( input_num.value.toString().includes("e")){
+        console.log("input should not contain e");
+    }
 }
 
-//called from zero-line submit button function pointer
-var submit = function(){
-	//format and submit the number to the API
+var button = document.createElement("button");
+button.innerText = "SUBMIT";
+button.addEventListener("click", verify);
+body.appendChild(button);
 
-}
-
-function _zero_line(){
-	var row = document.createElement("div");
-	body.appendChild(row);
-	for(var x = 0; x < 3; x++){
-		console.log(x + "0");
-		buttons[3][x] = {
-			value: 1 + x + (y*3),
-			domNode: document.createElement("button")
-		};
-		switch(x){
-			case 0: buttons[3][x].domNode.innerText = "*";
-				buttons[3][x].value = "*"; break;
-			case 1: buttons[3][x].domNode.innerText = "0"; 
-				buttons[3][x].value = 0; break;
-			case 2: buttons[3][x].domNode.innerText = "#"; 
-				buttons[3][x].value = "#"; break;
-			default: console.log("error in _zero_line");
-		}
-//		console.log("buttons[3][" + x + "] value is " + buttons[3][x].value);
-		body.appendChild(buttons[3][x].domNode);
-//		buttons[3][x].domNode.onclick = clicker;
-		buttons[3][x].domNode.onclick = clicker.bind(null, buttons[3][x].value);
-	}
-//work on the submit function
-//	buttons[4][0]
-}
-
-//setup numpad
-for(var y = 0; y < 3; y++) {
-    var row = document.createElement("div");
-	body.appendChild(row);
-	for( var x = 0; x < 3; x++){
-		buttons[x][y] = {
-			value: 1 + x + (y*3),
-			domNode: document.createElement("button")
-		};
-			
-		buttons[x][y].domNode.innerText = buttons[x][y].value;
-		body.appendChild(buttons[x][y].domNode);
-//		buttons[x][y].domNode.onclick = clicker;
-		buttons[x][y].domNode.onclick = clicker.bind(null, buttons[x][y].value);
-
-		//zero is a pain in the ass
-		if(1 + x + (y*3) == 9){
-			_zero_line();
-		}
-	}
-}
+window.addEventListener("keyup", function(e) {
+    var key = e.which || e.code;
+    if(key == 13){
+        verify();
+    }
+})
