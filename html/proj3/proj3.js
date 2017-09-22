5cc51d2c0a9db90034a43d54d4fcc016e201b2e4
mfergu <mfergu3@g.clemson.edu>
2017-09-22 10:00:04 -0400

    ready for testing

--- html/proj3/proj3.js
+++ html/proj3/proj3.js
@@ -1,81 +1,139 @@
-console.log("connected!");
-
 var body = document.querySelector("body");
 var input_num;
-var access_key = "b1d5f828832403389f1e161a42f63498";
-var get_string;
-var api_return;
 
-//create title
-document.querySelector("title").innerText = " Project 3";
+var numv_key = "b1d5f828832403389f1e161a42f63498";
+var numv_get_string;
+var numv_return;
 
-//create user input
-var num_input = document.createElement("input");
-num_input.type = "number";
-num_input.pattern = "[0-9]";
-num_input.max = "999999999999999";
-body.appendChild(num_input);
+var gmaps_key = "AIzaSyCI5Bi09F2NwleFuBVgdec-F3L8-6OODP0";
+var gmaps_get_string;
+var gmaps_return;
 
-//verify user input 
-var verify = function(){
-    input_num = document.querySelector("input");
-    console.log(input_num.value);
-    if( input_num.value.toString().length > 15 ) {
-        console.log("input too long");
-    } else if( input_num.value.toString().includes("e")){
-        console.log("input should not contain e");
-    }
-    get_string = 'http://apilayer.net/api/validate?access_key=' + access_key 
-        + '&number=' + input_num.value;
+function start(){
+    //create user input
+    var user_input = document.createElement("input");
+    user_input.type = "number";
+    user_input.pattern = "[0-9]";
+    user_input.max = "999999999999999";
+    body.appendChild(user_input);
+
+
+    var button = document.createElement("button");
+    button.innerText = "SUBMIT";
+    button.addEventListener("click", prepareNumverify);
+    body.appendChild(button);
+    var stat = document.createElement("h2");
+    body.appendChild(stat);
+
+    window.addEventListener("keyup", function(e) {
+        var key = e.which || e.code;
+        if(key == 13){
+            prepareNumverify();
+        }
+    })
 }
 
-var button = document.createElement("button");
-button.innerText = "SUBMIT";
-button.addEventListener("click", verify);
-body.appendChild(button);
-
-window.addEventListener("keyup", function(e) {
-    var key = e.which || e.code;
-    if(key == 13){
-        verify();
+function createGmap(){
+    var gmap = document.querySelector("iframe");
+    if(gmap == null){
+        gmap = document.createElement("iframe");
     }
-})
+    gmap.width="600";
+    gmap.height="450";
+    gmap.frameborder="0";
+    gmap.stype="border:0";
+    gmap.src = gmaps_get_string;
+    gmap.innerHTML += " allowfullscreen";
+    console.log(gmap);
+    body.appendChild(gmap);
+}
 
+function prepareGmaps(){
+    gmaps_get_string = "https://www.google.com/maps/embed/v1/place?key=" + gmaps_key
+                + "&q=" + numv_return.country_name;
+            gmaps_get_string = gmaps_get_string.replace(/ /g,"+");
+            console.log(gmaps_get_string);
+            createGmap();
+}
 
+function printEmptyStrings(ret){
+    if(document.querySelector("ul")){
+        var temp = document.querySelector("ul");
+        temp.parentNode.removeChild(temp);
+    }
+    var leest = document.createElement("ul");
+    var leest_item = document.createElement("li");
+    if(ret.carrier === ""){
+        leest_item.innerText = " missing carrier ";
+        leest.appendChild(leest_item);
+    }
+    if(ret.country_code ===""){
+        leest_item.innerText = " missing country_code ";
+    }
+    if(ret.country_name ===""){
+        leest_item.innerText = " missing country_name ";
+        leest.appendChild(leest_item);
+    }
+    if(ret.country_prefix ===""){
+        leest_item.innerText = " missing country_prefix ";
+        leest.appendChild(leest_item);
+    }
+    if(ret.local_format ===""){
+        leest_item.innerText = " missing local_format ";
+        leest.appendChild(leest_item);
+    }
+    if(ret.international_format ===""){
+        leest_item.innerText = " missing international_format ";
+        leest.appendChild(leest_item);
+    }
+    if(ret.line_type ===""){
+        leest_item.innerText = " missing line_type ";
+        leest.appendChild(leest_item);
+    }
+    if(ret.location ===""){
+        leest_item.innerText = " missing location ";
+        leest.appendChild(leest_item);
+    }
+    if(ret.number ===""){
+        leest_item.innerText = " missing number ";
+        leest.appendChild(leest_item);
+    }
+    body.appendChild(leest);
+}
 
+function submitNumverify(){
+    numv_return = axios.get(numv_get_string)
+        .then(function(response) {
+            numv_return = response.data;
 
-var promise1 = new Promise(function(resolve, reject) {
-    //do a thing async
-        var httpRequest;
-        function makeRequest(){
-            httpRequest = new XMLHttpRequest();
-
-            if(!httpRequest) {
-                    alert("cannot create an XMLHTTP instance");
-            }
-            httpRequest.onreadstatechange = alertContents;
-            httpRequest.open('GET', get_string );
-            httpRequest.send();
-        }
-
-        function alertContents() {
-            if(httpRequest.readyState == XMLHttpRequest.DONE){
-                if(httpRequest.status === 200) {
-                    alert(httpRequest.responseText);
-                } else {
-                    alert("there was a problem with the request");
+            //update status
+            if(numv_return.valid == true){
+                document.querySelector("h2").innerText = "result is a valid " + numv_return.line_type
+                + " number" ;
+                printEmptyStrings(numv_return);
+                prepareGmaps();
+            } else {
+                if(document.querySelector("iframe")){
+                    var frame = document.querySelector("iframe");
+                    frame.parentNode.removeChild(frame);
                 }
-            }
-        }
-    //if everything turned out fine
-    if(httpRequest!=false){
-        api_return = httpRequest;
-        resolve("stuff worked!");
+                document.querySelector("h2").innerText = "result is invalid";  
+            }   
+        }).catch(function(error) {
+         console.log(error);   
+        });
+}
 
-    } else {
-        reject(Error("It broke!"));
-       
-    }
-});
+//prepare numverify user input 
+var prepareNumverify = function(){
+    input_num = document.querySelector("input");
+    numv_get_string = 'http://apilayer.net/api/validate?access_key=' + numv_key 
+        + '&number=' + input_num.value;
+    submitNumverify();
+}
 
-var promise2 = promise1.then(console.log(api_return));
+window.addEventListener("load", function(){start()}, false);
+
+
+
+
